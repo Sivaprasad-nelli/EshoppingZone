ce71eadf4946a73c60bb5c3ccb370174eb68ef3e
Sivaprasad-nelli <96195919+Sivaprasad-nelli@users.noreply.github.com>
2022-01-24 08:26:24 +0530

    first commit

---
+++ clientside/src/features/account/accountSlice.ts
@@ -0,0 +1,89 @@
+import { User } from "../../app/models/user";
+import {createAsyncThunk, createSlice, isAnyOf} from "@reduxjs/toolkit";
+import { FieldValues } from "react-hook-form";
+import apihandler from "../../app/api/apihandler";
+import { history } from "../..";
+
+interface AccountState {
+    user:User |null
+}
+
+const initialState: AccountState = {
+    user:null
+}
+
+export const signInUser = createAsyncThunk<User, FieldValues>(
+    'account/signInUser',
+    async (data, thunkAPI) => {
+        try {
+            const user = await apihandler.Account.login(data);
+            // const {basket, ...user} = userDto;
+            localStorage.setItem('user', JSON.stringify(user));
+            return user;
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({error: error.data});
+        }
+    }
+)
+
+export const fetchCurrentUser = createAsyncThunk<User>(
+    'account/signInUser',
+    async (_, thunkAPI) => {
+        try {
+            const user = await apihandler.Account.currentUser();
+            // const {basket, ...user} = userDto;
+            localStorage.setItem('user', JSON.stringify(user));
+            return user;
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({error: error.data});
+        }
+    }
+)
+
+// export const accountSlice = createSlice(
+
+//     {
+//         name:'account',
+//         initialState,
+//         reducers:{},
+//         extraReducers:(builder => 
+            // {
+            // builder.addMatcher(isAnyOf(signInUser.fulfilled, fetchCurrentUser.fulfilled), (state, action) => {
+            //     state.user = action.payload;
+            // })
+//         builder.addMatcher(isAnyOf(signInUser.rejected), (state, action) => {
+//                 //  throw action.payload;
+//                 console.log(action.payload);
+//             })
+        
+//     }
+
+
+// )
+
+export const accountSlice = createSlice({
+    name: 'account',
+    initialState,
+    reducers: {
+
+        signOut: (state) => {
+            state.user = null;
+            localStorage.removeItem('user');
+            history.push('/')
+        },
+    },
+    extraReducers:(
+
+        builder =>{
+            builder.addMatcher(isAnyOf(signInUser.fulfilled, fetchCurrentUser.fulfilled), (state, action) => {
+                state.user = action.payload;
+            });
+            builder.addMatcher(isAnyOf(signInUser.rejected), (state, action) => {
+                console.log(action.payload);
+            })
+        }
+    )
+})
+
+export const {signOut} = accountSlice.actions; 
+    
